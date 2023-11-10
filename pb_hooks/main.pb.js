ef72c0e80b3d416b80d060e2ee9f7a315765b5a4
Juan Manuel Sabanes <jmsabanes07@gmail.com>
2023-11-09 22:48:17 -0300

    a

--- pb_hooks/main.pb.js
+++ pb_hooks/main.pb.js
@@ -4,4 +4,9 @@ onMailerBeforeRecordResetPasswordSend(e => {
   console.log(e.record)
   console.log(e.meta)
   console.log(JSON.stringify(e))
+
+  const newHtml = e.message.html.replace('&app=', '&app=Chofer')
+
+  // change the mail subject
+  e.message.html = newHtml
 })
