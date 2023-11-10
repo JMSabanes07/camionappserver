cfa890a53308eceef78d18fe8acd7e27a23d6de2
Juan Manuel Sabanes <jmsabanes07@gmail.com>
2023-11-09 23:07:09 -0300

    a

--- pb_hooks/main.pb.js
+++ pb_hooks/main.pb.js
@@ -1,12 +1,25 @@
 onMailerBeforeRecordResetPasswordSend(e => {
-  console.log(e.mailClient)
-  console.log(e.message)
-  console.log(e.record)
-  console.log(e.meta)
-  console.log(JSON.stringify(e))
+  console.log(JSON.stringify(e.mailClient))
+  console.log(JSON.stringify(e.message))
+  console.log(JSON.stringify(e.record))
+  console.log(JSON.stringify(e.meta))
 
   const newHtml = e.message.html.replace('&app=', `&app=${e.collection.name}`)
 
   // change the mail subject
   e.message.html = newHtml
+
+  console.log(`
+  
+  Nuevo Html -----------------------------
+
+  ${JSON.stringify(newHtml)}
+
+  End line --------------------------------------
+
+  `)
+})
+
+onMailerAfterRecordResetPasswordSend(e => {
+  console.log(JSON.stringify(e))
 })
